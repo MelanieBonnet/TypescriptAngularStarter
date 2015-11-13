a2305acc728e26ab68727c590fbafa2935dcab14
Benoit Verdier <benoit.verdier@3ie.fr>
2015-11-13 11:20:26 +0100

    new grunt file that run usemin, compiles less, uses filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,144 @@
-/**
- * Created by arnaud lemettre on 22/10/2015.
- */
-
 module.exports = function (grunt) {
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
-  var globalConfig = {
-    tsFiles: ['**/*.ts', '!node_modules/**/*.ts', '!bower_components/**/*.ts', '!typings/**/*.ts'],
-    jsFiles: ['**/*.js', '!node_modules/**/*.js', '!bower_components/**/*.js']
-  };
+	// load all grunt tasks without explicitly referecing them
+	require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    globalConfig: globalConfig,
-    ts: {
-      default: {
-        src: '<%= globalConfig.tsFiles %>',
-        options: {
-          target: 'es5',
-          module: 'commonjs',
-          noEmitOnError: true,
-          fast: 'never'
-        }
-      }
-    },
-    tslint: {
-      options: {
-        configuration: grunt.file.readJSON("tslint.json")
-      },
-      files: {
-        src: '<%= globalConfig.tsFiles %>'
-      }
-    },
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: '<%= globalConfig.tsFiles %>',
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    clean: { 
-      // remove old javascript files 
-      public: [
-        "app/**/*.{js,js.map}",
-        "app_engine/**/*.{js,js.map}",
-        "reference.{js,js.map}",
-        "models/**/*.{js,js.map}"
-      ]
-    },
-    'http-server': {
-      'dev': {
-        port: 5000,
-        openBrowser: true
-      }
-    }
-  });
-  
-  // Default task(s).
-  grunt.registerTask('web', ['http-server:dev']);
-  grunt.registerTask('default', ['clean', 'tslint', 'ts']);
+	var path = require('path');
+	var globalCfg = {
+		src: {
+			tsFiles: ['{app,app_engine,models}/**/*.ts'],
+			staticMiscFiles: ['index.html', 'app/**/*.{html,json}', 'app/img/**'],
+			staticFontFiles: ['bower_components/bootstrap/dist/fonts/**']
+		},
+		distDir: 'dist/'
+	};
+	
+	//less config function for usemin
+	var lessCreateConfig = function (context, block) {
+		var cfg = { files: [] },
+			outfile = path.join(context.outDir, block.dest),
+			filesDef = {};
 
+		filesDef.dest = outfile;
+		filesDef.src = [];
+
+		context.inFiles.forEach(function (inFile) {
+			filesDef.src.push(path.join(context.inDir, inFile));
+		});
+
+		cfg.files.push(filesDef);
+		context.outFiles = [block.dest];
+		return cfg;
+	};
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		globalCfg: globalCfg,
+		ts: {
+			default: {
+				src: '<%= globalCfg.src.tsFiles %>',
+				options: {
+					target: 'es5',
+					module: 'commonjs',
+					noEmitOnError: true,
+					fast: 'never'
+				}
+			}
+		},
+		tslint: {
+			options: {
+				configuration: grunt.file.readJSON("tslint.json")
+			},
+			files: {
+				src: '<%= globalCfg.src.tsFiles %>'
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{ expand: true, src: '<%= globalCfg.src.staticMiscFiles %>', dest: '<%= globalCfg.distDir %>', filter: 'isFile' },
+					{ expand: true, src: '<%= globalCfg.src.staticFontFiles %>', dest: '<%= globalCfg.distDir %>/fonts/', flatten: true, filter: 'isFile' }
+				],
+			},
+		},
+		useminPrepare: {
+			//html file that usemin is going to analyse to find the files to process
+			html: 'app/index.html',
+			options: {
+				//destination folder that usemin is going to use to output the processed files (for .js and .css for example)
+				dest: '<%= globalCfg.distDir %>/app/',
+				//this custom flow allows usemin to support less
+				flow: {
+					steps: {
+						'js': ['concat', 'uglify'],
+						'css': ['concat', 'cssmin'],
+						'less': [{
+							name: 'less',
+							createConfig: lessCreateConfig
+						}]
+					},
+					post: {}
+				},
+			}
+		},
+		less: {
+			options: {
+				plugins: [
+					//plugin that automatically prefixes the css rules with vendor prefixes when needed 
+					new (require('less-plugin-autoprefix'))({ browsers: ["last 2 versions"] }),
+					//plugin that minifies the css
+					new (require('less-plugin-clean-css'))()
+				]
+			}
+		},
+		usemin: {
+			//html file within which usemin is going to replace the resource references  
+			html: ['<%= globalCfg.distDir %>/app/**/*.html'],
+			options: {
+				assetsDirs: '<%= globalCfg.distDir %>/app',
+				blockReplacements: {
+					less: function (block) {
+						return '<link rel="stylesheet" href="' + block.dest + '">';
+					}
+				}
+			}
+		},
+		filerev: {
+			options: {
+				algorithm: 'md5',
+				length: 8
+			},
+			images: { src: '<%= globalCfg.distDir %>/app/img/**/*' },
+			js: { src: '<%= globalCfg.distDir %>/app/js/**/*' },
+			css: { src: '<%= globalCfg.distDir %>/app/css/**/*' }
+		},
+		clean: {
+			public: [
+				'{app,app_engine,models}/**/*.{js,js.map}',
+				'<%= globalCfg.distDir %>/**',
+				'reference.{js,js.map}'
+			]
+		},
+		'http-server': {
+			'dev': {
+				root: '<%= globalCfg.distDir %>',
+				port: 5000,
+				openBrowser: true
+			}
+		}
+	});
+
+	grunt.registerTask('processTypescript', ['tslint', 'ts']);
+	grunt.registerTask('build', [
+		'clean',
+		'copy',
+		'processTypescript',
+		'useminPrepare',
+		'less:generated',
+		'cssmin:generated',
+		'concat:generated',
+		'uglify:generated',
+		'filerev',
+		'usemin'
+	]);
+	grunt.registerTask('web', ['http-server:dev']);
 };
