51efaf764ff15abf80a27fea43f6aa56ce8fcb60
Benoit Verdier <benoit.verdier@3ie.fr>
2016-02-05 18:13:27 +0100

    fixed typings reference

--- test/reference.ts
+++ test/reference.ts
@@ -1,3 +1,1 @@
-/// <reference path="../typings/jasmine/jasmine.d.ts"/>
-/// <reference path="../typings/angularjs/angular.d.ts"/>
-/// <reference path="../typings/angularjs/angular-mocks.d.ts"/>
+/// <reference path="../typings/main.d.ts"/>
